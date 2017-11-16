07e1ce0078f80fe58dc69a5f3e13b81577b362bb
cwillhelm <connor.willhelm@gmail.com>
2017-11-15 22:19:41 -0500

    Changes to be committed: 	modified:   gen.py

--- gen.py
+++ gen.py
@@ -14,24 +14,23 @@ length = len(sys.argv)
 
 # exit if user input was not input correctly
 if length > 3:
-    print ("Invalid argument. The syntax is pygen.py <stringLength> <numberOfPasswords>")
+    print ("Invalid argument. The syntax is gen.py <stringLength> <numberOfPasswords>")
     sys.exit(0)
 
-# error handling, defaults to a string length of 12 
+# error handling, defaults to a string length of 14
 try:
     n = int(sys.argv[1]) # string length
     x = int(sys.argv[2]) # number of passwords to generate
 except IndexError:
-    n = 14 # default to a string length of 12
+    n = 14 # default to a string length of 14
     x = 1  # default to one password
 
 print ('-' * n)
+
+### TODO: Remove the blank line that appears after the - char
 
 # generate a random string based on the length of the user input
 for _ in range(x):
     print ('')
     for i in range(n):
         print ((random.SystemRandom().choice(chars)), end='') 
-        
-    
-
